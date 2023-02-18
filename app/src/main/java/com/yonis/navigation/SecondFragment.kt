7da4c2a065cea979d7cc7338a1cdbe0922fce3dc
Yunus Emre Ceylan <yec2442@gmail.com>
2023-02-18 16:25:05 +0300

    commited

---
+++ app/src/main/java/com/yonis/navigation/SecondFragment.kt
@@ -0,0 +1,38 @@
+package com.yonis.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            val nick = SecondFragmentArgs.fromBundle(it).nick
+            println(nick)
+            textView.text = nick
+        }
+        button2.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragmnet()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
